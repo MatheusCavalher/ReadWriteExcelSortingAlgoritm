0bfde6f5916d5d479b77133a44d412923fc9c3f5
Matheus Cavalher <50085058+MatheusCavalher@users.noreply.github.com>
2019-10-13 19:07:03 -0300

    Add files via upload

---
+++ ExcelLerEEscrever/ExcelLerEEscrever/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelLerEEscrever
+{
+    class BubbleSort
+    {
+
+        public int[] ordena(int[] vetor)
+        {
+            int tamanho = vetor.Length;
+            int comparacoes = 0;
+            int trocas = 0;
+
+            for (int i = tamanho - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    comparacoes++;
+
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        int aux = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = aux;
+                        trocas++;
+                    }
+                }
+            }
+            return vetor;
+        }
+    }
+}
---
+++ ExcelLerEEscrever/ExcelLerEEscrever/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using IronXL;
+
+namespace ExcelLerEEscrever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetorDesordenado = new int[1001];
+            int[] vetorGuarda = new int[1001];
+            int[] vetorQuickSortORdenado;
+            int[] vetorBubbleSortORdenado;
+            QuickSort quicksort = new QuickSort();
+            BubbleSort bubblesort = new BubbleSort();
+            
+            WorkBook workbook = WorkBook.Load(@"ordenacao.xlsx");
+            WorkSheet sheet = workbook.WorkSheets.First();
+
+            for (int i = 1; i <= 1000; i++) {
+                vetorDesordenado[i] = sheet["A" + i].IntValue;
+                vetorGuarda[i] = sheet["A" + i].IntValue;
+                //Console.WriteLine("Vetor Desordenado " + i + " : " + vetorDesordenado[i]);
+            }
+
+            //verificando tempo decorrido na execucao da ordenacao dometodo QuickSort
+            var stopwatchQuick = new Stopwatch();
+            stopwatchQuick.Start();
+            vetorQuickSortORdenado = quicksort.ordena(vetorDesordenado);
+            stopwatchQuick.Stop();
+            Console.WriteLine($"Tempo passado QuickSort: {stopwatchQuick.Elapsed}");
+
+            //verificando tempo decorrido na execucao da ordenacao do metodo BubbleSort
+            var stopwatchBubble = new Stopwatch();
+            stopwatchBubble.Start();
+            vetorBubbleSortORdenado = bubblesort.ordena(vetorDesordenado);
+            stopwatchBubble.Stop();
+            Console.WriteLine($"Tempo passado Bubble Sort: {stopwatchBubble.Elapsed}");
+
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                //Console.WriteLine("Vetor Ordenado QuickSort " + i + " : " + vetorQuickSortORdenado[i]);
+            }
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                //Console.WriteLine("Vetor Ordenado BubbleSort " + i + " : " + vetorBubbleSortORdenado[i]);
+            }
+
+            //Criando um arquivo Excel e exportando na primeira coluna os valores desordenados e na segunda coluna 
+            //com os valores ordenados
+            WorkBook workbookSave = WorkBook.Create(ExcelFileFormat.XLSX);
+            var sheetSave = workbookSave.CreateWorkSheet("example_sheet");
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                sheetSave["A" + i].IntValue = vetorGuarda[i];
+            }
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                sheetSave["B" + i].IntValue = vetorBubbleSortORdenado[i];
+            }
+
+            workbookSave.SaveAs(@"ordenacao2.xlsx");
+        }
+    }
+}
